9e725710747a923ec17645e99166e2c8c5f6855e
Elizoreath <103290606+Elizoreath@users.noreply.github.com>
2022-06-02 17:24:06 -0400

    Added Song
    I added in my song. :)

--- GameWorld.java
+++ GameWorld.java
@@ -10,21 +10,24 @@ public class GameWorld extends World
 {
     Label scoreLabel;
     public int score = 0;
-    
+    GreenfootSound song = new GreenfootSound("Clock Tower (Remix).mp3");
+
     public GameWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1, false); 
 
+        song.play();
+        
         UserGood click = new UserGood();
         addObject(click, 100, 200);
-        
+
         createBlue();
 
         //Create a label for score
         scoreLabel = new Label(0, 40);
         addObject(scoreLabel, 100, 150);
-        
+
         //Creates a TimeKeeper
         createTimer();
     }
@@ -45,7 +48,7 @@ public class GameWorld extends World
         Blue blueSquare = new Blue();
         addObject(blueSquare, 600, 200);
     }
-    
+
     //Increase score by 10
     public void increaseScoreGood()
     {
